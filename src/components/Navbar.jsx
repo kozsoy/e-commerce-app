b7ae63b68faa537e9bb759547bbd3d5c20a0587f
kozsoy <kadirozsoy888@gmail.com>
2023-10-03 13:37:52 +0300

    rev+

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,49 @@
 import React from "react";
-import {BsLightbulb} from 'react-icons/bs'
+import { useState , useEffect } from "react";
+import { BsLightbulb, BsBasket , BsMoonStarsFill } from "react-icons/bs";
+
 const Navbar = () => {
+  const [color , setColor] = useState(false)
+  
+  
+  useEffect(()=>{
+  const root = document.getElementById('root')
+  
+    if(color){
+      root.style.backgroundColor = "black";
+      root.style.color="gray";
+  }else{
+    root.style.backgroundColor="white";
+    root.style.color="black"
+  }
+  
+  },[color])
+  
+  
+  
   return (
-  
-  <div>
-    <div>LOGO</div>
-    <div>
-        <input type="text" placeholder="Search" />
+    <div className="flex items-center justify-between px-3 h-20">
+      <div>LOGO</div>
+      <div>
+        <input className="border p-1 outline-none rounded-md" type="text" placeholder="Search" />
+      </div>
+      <div onClick={()=>setColor(!color)}>
+
+        {
+          color ? < BsMoonStarsFill size={25} className="cursor-pointer" /> : < BsLightbulb size={25} className="cursor-pointer" />
+        }
+           
+        
+        
+      </div>
+      <div className="relative">
+        <BsBasket size={25} className="cursor-pointer" />
+        <span className=" absolute -top-2 -right-3 px-2 text-white bg-red-600 rounded-full text-sm">
+          3
+        </span>
+      </div>
     </div>
-    <div> 
-    <BsLightbulb />
-    </div>
-  </div>
-  
-  )
+  );
 };
 
 export default Navbar;
