a77f0dd7bc28857464930942a04df7a9ee16e8ab
kozsoy <kadirozsoy888@gmail.com>
2023-10-06 21:44:28 +0300

    06.10.04

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -6,7 +6,8 @@ import React from 'react'
 const ProductsCard = ({prod }) => {
   return (
     <div className='w-1/5 h-[250px] border-solid border-2 border-sky-500  rounded-lg m-2'  >
-
+        <img src={prod?.image} className='cursor-pointer' />
+        
     </div>
   )
 }
