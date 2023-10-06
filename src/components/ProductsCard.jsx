e31b9d349009fcd949cacb069cc72d3be720a7d9
kozsoy <kadirozsoy888@gmail.com>
2023-10-06 21:13:10 +0300

    06.10.03

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const ProductsCard = ({prod }) => {
   return (
-    <div className='w-1/5 h-[350px] border rounded-lg m-2'  >
+    <div className='w-1/5 h-[250px] border-solid border-2 border-sky-500  rounded-lg m-2'  >
 
     </div>
   )
